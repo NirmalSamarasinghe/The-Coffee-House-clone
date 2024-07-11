9a94b9854308f61a2b93855f731ca907665f5b6c
Nipun78 <nipunnirmal67@gmail.com>
2024-07-11 22:38:34 +0530

    project init & done the Loading

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import Loading from "./Loading";
+
+export default function Index() {
+  return (
+    <>
+      <View>
+        <Text>Welcome to the Index Page!</Text>
+        </View>
+    </>
+    
+  );
+}
